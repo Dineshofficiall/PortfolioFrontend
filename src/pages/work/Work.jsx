8a8452c428654768424f6c847e442ddb7fb41233
Dinesh Kumar <dineshsaravananofficial@gmail.com>
2024-09-30 18:00:46 +0530

    workExxperience component content completed

--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -1,7 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { Col, Container, Image, Row } from 'react-bootstrap';
-import amcIcon from '/Project/PortfolioFrontend/Portfolio/src/assets/education logo/amc.png';
+import hemaIcon from '/Project/PortfolioFrontend/Portfolio/src/assets/work/hemaLogo.webp';
+import './Work.css';
 
 function Work() {
     return (
@@ -9,22 +10,26 @@ function Work() {
             <Container className='col-12 col-md-10 col-lg-7'>
                 <div className='text-center mb-5'>
                     <h1>Work Experience </h1>
-                    <article>My work experience as a full stack engineer and working on different companies and projects.</article>
+                    <article>My work experience as a full stack engineer and working on companies and projects.</article>
                 </div>
                 <Row className='col-12 ms-2 contents'>
                     <Col className='col-12 border rounded-4 p-3 mb-4 box'>
                         <div className='d-flex align-items-center'>
-                            <Image src={amcIcon} width={45} className='me-3' alt='' />
+                            <Image src={hemaIcon} width={45} className='me-3' alt='' />
                             <div>
-                                <h5 className='m-0'>The American College of Arts and Science, TamilNadu</h5>
-                                <p className='m-0'>Bachelor of Science - BSc, Computer Science</p>
+                                <h5 className='m-0'>Hema enterprise, Cuddalore TamilNadu</h5>
+                                <small className='m-0'>ReactNative Developer - Mobile Development</small>
                             </div>
                         </div>
                         <div className='mt-3 mb-2'>
-                            <h6 className='m-0'>Grade: 7.1 CGPA</h6>
-                            <em style={{ fontSize: "12.3px", fontWeight: "lighter" }}>Jul 2019 - May 2022</em>
+                            <em style={{ fontSize: "12.3px", fontWeight: "lighter" }}>Sep 2024 - Present</em>
                         </div>
-                        <article>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat commodi neque nihil dignissimos magnam quo tempore ipsa quibusdam eos, error porro molestiae labore quae!</article>
+                        <ul>
+                            <li className='list-block'>In my role as a React Native frontend developer at Hema Enterprise Pvt Ltd for the past 2 months, I have gained profound insights into system architecture, particularly the MVVM (Model-View-ViewModel) design pattern.</li>
+                            <li className='list-block'>Collaborating with seasoned professionals has significantly deepened my comprehension of best practices in application development.</li>
+                            <li className='list-block'>I successfully completed an independent project focused on event management, which allowed me to effectively apply my skills and creativity.</li>
+                            <li className='list-block'>Following this project, I advanced to real-time initiatives, including the development of a meeting agenda and a form builder, which further enriched my practical experience in crafting responsive mobile applications.</li>
+                        </ul>
                     </Col>
                 </Row>
             </Container>
